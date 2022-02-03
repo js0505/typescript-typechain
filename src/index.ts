2b0536641ede0a1af24bec649f61ca12a05c8b4f
js0505 <shinjesoo@gmail.com>
2022-02-03 23:25:22 +0900

    ts-watch 실시간으로 터미널에 표시. src ->ts,  dist->컴파일된 파일

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+
+const sayHi = (name:string, age:number, gender:string): void => {
+    console.log(`Hello ${name}, you are ${age}, you are a ${gender}!`);
+};
+
+sayHi("jisu", 32, "male");
+
+export { };
