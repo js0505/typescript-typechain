62c659ec627b3bb9494f637566a08c8faf2d35c0
js0505 <shinjesoo@gmail.com>
2022-02-04 23:07:02 +0900

    class

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,28 @@
-interface Human {
-    name: string;
-    age: number;
-    gender: string;
+class Human {
+    public name: string;
+    public age: number;
+    public gender: string;
+
+    // private 속성의 변수는
+    // sayHi 함수처럼 인자를 통해서 변수에 접근 할 수 없다.
+    private canNotUse: string;
+
+    // 생성자 : 클래스로부터 객채를 만들 때 마다 실행되는 메소드
+    constructor(name: string, age: number, gender: string, canNotUse?: string) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+        this.canNotUse = canNotUse;
+    }   
 }
 
-const person = {
-    name: 'jisu',
-    age: 32,
-    gender: 'male',
-};
+const jisu = new Human("jisu", 32, "male");
 
 
 const sayHi = (person: Human): void => {
     console.log(`Hello ${person.name}, you are ${person.age}, you are a ${person.gender}!`);
 };
 
-console.log(sayHi(person));
+console.log(sayHi(jisu));
 
 export { };
